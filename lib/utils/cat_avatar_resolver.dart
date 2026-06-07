aa52f24734c14bf5a154ce406860a5019179dc8c
sywer <sywer@protonmail.com>
2026-06-07 13:21:31 +0300

    fix misprint

--- lib/utils/cat_avatar_resolver.dart
+++ lib/utils/cat_avatar_resolver.dart
@@ -2,7 +2,7 @@ import 'package:cat_calories_core/features/profile/domain/profile.dart';
 import 'package:flutter/widgets.dart';
 
 class CatAvatarResolver {
-  static AssetImage getImageByProfle(Profile profile) {
+  static AssetImage getImageByProfile(Profile profile) {
     final digit = (profile.id.hashCode.abs() % 10).toString();
 
     return AssetImage('images/cats/cat_face_0$digit.jpg');
